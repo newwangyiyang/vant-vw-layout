2b81e1ee26ecae51a4a52b129735ba9d7a1267cc
王一杨 <wangyiyang@163.com>
2018-03-20 15:08:57 +0800

    有赞商城的vant模板

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//样式初始化
+import '../static/css/reset.css';
+//引入axios请求
+import axios from './axios/http';
+Vue.prototype.$http = axios;
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
